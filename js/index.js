c65a97171ced1adb6bd2eaac4aa1e2ba1c727754
Armel Larcier <armel.larcier@gmail.com>
2017-08-13 15:40:19 +0200

    build

--- js/index.js
+++ js/index.js
@@ -1,1 +1,1 @@
-!function(r){function t(o){if(e[o])return e[o].exports;var n=e[o]={exports:{},id:o,loaded:!1};return r[o].call(n.exports,n,n.exports,t),n.loaded=!0,n.exports}var e={};return t.m=r,t.c=e,t.p="",t(0)}([function(r,t){"use strict"}]);
+!function(r){function o(e){if(t[e])return t[e].exports;var n=t[e]={exports:{},id:e,loaded:!1};return r[e].call(n.exports,n,n.exports,o),n.loaded=!0,n.exports}var t={};return o.m=r,o.c=t,o.p="",o(0)}([function(r,o){"use strict";console.log("2017")}]);
